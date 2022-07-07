5c0e2b573b924fb4d8ae0a0c8dd9d9bee7acebaf
Blake Saylor <blakesaylor@gmail.com>
2022-07-06 18:19:31 -0600

    Add test for first user story

--- spec/features/areas/index_spec.rb
+++ spec/features/areas/index_spec.rb
@@ -1,18 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe 'areas index' do
+RSpec.describe 'areas index', type: :feature do
     # User Story 1, Parent Index 
     # For each parent table
     # As a visitor
     # When I visit '/parents'
     # Then I see the name of each parent record in the system
     it 'returns the name of each area in system on the index page' do
-        area = Area.create(name:'Clear Creek Canyon', state:'Colorado', rock_climbing: true, elevation: 7400, latitude: 39.741, longitude: -105.41)
+        area_1 = Area.create!(name:'Clear Creek Canyon', state:'Colorado', rock_climbing: true, elevation: 7400, latitude: 39.741, longitude: -105.41)
+        area_2 = Area.create!(name:'Boulder Canyon', state:'Colorado', rock_climbing: true, elevation: 7126, latitude: 40.002, longitude: -105.41)
 
-        visit "/areas"
+        visit '/areas'
 
         save_and_open_page
 
-        expect(page).to have_content(area.name)
+        expect(page).to have_content(area_1.name)
+        expect(page).to have_content(area_2.name)
     end
 end
