e8725c59eb18631aadb40a9264dbfe6a87f90065
Blake Saylor <blakesaylor@gmail.com>
2022-07-06 21:49:26 -0600

    Update object declaration

--- spec/features/climbs/index_spec.rb
+++ spec/features/climbs/index_spec.rb
@@ -6,8 +6,9 @@ RSpec.describe 'climbs index', type: :feature do
     # When I visit '/child_table_name'
     # Then I see each Child in the system including the Child's attributes:
     it 'shows the name of each climb in system on the index page' do
-        climb_1 = Climb.create!(name: "Playin' Hooky", lead:true, sport:true, trad:false, top_rope:false, class:5, grade:8, pitches:4)
-        climb_2 = Climb.create!(name: "Guppy", lead:true, sport:true, trad:false, top_rope:false, class:5, grade:8, pitches:1)
+        area_1 = Area.create!(name:'Clear Creek Canyon', state:'Colorado', rock_climbing: true, elevation: 7400, latitude: 39.741, longitude: -105.41)
+        climb_1 = area_1.climbs.create!(name: "Playin' Hooky", lead:true, sport:true, trad:false, top_rope:false, grade:'5.8', pitches:4)
+        climb_2 = area_1.climbs.create!(name: "Guppy", lead:true, sport:true, trad:false, top_rope:false, grade:'5.8', pitches:1)
 
         visit '/climbs'
 
