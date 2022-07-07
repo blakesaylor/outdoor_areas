56808c5d1a1807b527960853d7f963c4b372a0c3
Blake Saylor <blakesaylor@gmail.com>
2022-07-07 12:58:10 -0600

    Add route for finding climbs in an area

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get '/areas/:id', to: 'areas#show'
   get '/climbs', to: 'climbs#index'
   get '/climbs/:id', to: 'climbs#show'
+  get '/areas/:id/climbs', to: 'areas#show_area_climbs'
 end
