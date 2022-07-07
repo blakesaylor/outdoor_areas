9642d8b1d40c36971041cef38584213652b2baa0
Blake Saylor <blakesaylor@gmail.com>
2022-07-07 15:25:41 -0600

    Adds test for User Story 6

--- spec/features/areas/index_spec.rb
+++ spec/features/areas/index_spec.rb
@@ -15,4 +15,26 @@ RSpec.describe 'areas index', type: :feature do
         expect(page).to have_content(area_1.name)
         expect(page).to have_content(area_2.name)
     end
+
+    # User Story 6, Parent Index sorted by Most Recently Created 
+    # As a visitor
+    # When I visit the parent index,
+    # I see that records are ordered by most recently created first
+    # And next to each of the records I see when it was created
+    it 'shows all areas ordered by the created_at column' do
+        area_1 = Area.create!(name:'Clear Creek Canyon', state:'Colorado', rock_climbing: true, elevation: 7400, latitude: 39.741, longitude: -105.41)
+        area_2 = Area.create!(name:'Boulder Canyon', state:'Colorado', rock_climbing: true, elevation: 7126, latitude: 40.002, longitude: -105.41)
+
+        visit '/areas/'
+
+        save_and_open_page
+
+        within '#area-0' do
+            expect(page).to have_content(area_2.name)
+        end
+
+        within '#area-1' do
+            expect(page).to have_content(area_1.name)
+        end
+    end
 end
