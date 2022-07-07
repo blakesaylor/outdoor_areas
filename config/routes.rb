8919bbc79ee057de4d12702a8440c416f15ce336
Blake Saylor <blakesaylor@gmail.com>
2022-07-06 22:23:55 -0600

    Add route for climb by id

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get '/areas', to: 'areas#index'
   get '/areas/:id', to: 'areas#show'
   get '/climbs', to: 'climbs#index'
+  get '/climbs/:id', to: 'climbs#show'
 end
