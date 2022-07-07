3f99662bddfc82e63628ba4494e2a2b721f96c65
Blake Saylor <blakesaylor@gmail.com>
2022-07-06 18:56:33 -0600

    Create test for second user story

---
+++ spec/features/areas/show_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'areas show by id' do
+    # User Story 2, Parent Show 
+    # As a visitor
+    # When I visit '/parents/:id'
+    # Then I see the parent with that id including the parent's attributes:
+    # - data from each column that is on the parent table
+    it 'shows all of the attributes for an area when viewing /area/:id' do
+        area_1 = Area.create!(name:'Clear Creek Canyon', state:'Colorado', rock_climbing: true, elevation: 7400, latitude: 39.741, longitude: -105.41)
+
+        visit '/areas/:id'
+
+        expect(page).to have_content(area_1.id)
+        expect(page).to have_content(area_1.name)
+        expect(page).to have_content(area_1.state)
+        expect(page).to have_content(area_1.rock_climbing)
+        expect(page).to have_content(area_1.elevation)
+        expect(page).to have_content(area_1.latitude)
+        expect(page).to have_content(area_1.longitude)
+        expect(page).to have_content(area_1.created_at)
+        expect(page).to have_content(area_1.updated_at)
+    end
+end
