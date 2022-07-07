939d07b2be23877bf6ae1d8c73c4af4f4ce0f95a
Blake Saylor <blakesaylor@gmail.com>
2022-07-06 21:13:33 -0600

    Update variable declaration

--- spec/features/climbs/index_spec.rb
+++ spec/features/climbs/index_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'climbs index', type: :feature do
     # Then I see each Child in the system including the Child's attributes:
     it 'shows the name of each climb in system on the index page' do
         climb_1 = Climb.create!(name: "Playin' Hooky", lead:true, sport:true, trad:false, top_rope:false, class:5, grade:8, pitches:4)
-        climb_2 = Climb.create!(name: "Guppy", lead:true, sport:true, trad:false, top_rope:false, class:5, grade:8, pitches:4)
+        climb_2 = Climb.create!(name: "Guppy", lead:true, sport:true, trad:false, top_rope:false, class:5, grade:8, pitches:1)
 
         visit '/climbs'
 
