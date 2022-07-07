feb1be91226c1adf4c16b25587a2e1597b7b6c65
Blake Saylor <blakesaylor@gmail.com>
2022-07-06 21:06:45 -0600

    Create test for User Story 3

---
+++ spec/features/climbs/index_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'climbs index' do
+    # User Story 3, Child Index 
+    # As a visitor
+    # When I visit '/child_table_name'
+    # Then I see each Child in the system including the Child's attributes:
+    it 'shows the name of each climb in system on the index page' do
+        climb_1 = Climb.create!(name: "Playin' Hooky", lead:true, sport:true, trad:false, top_rope:false, class:5, grade:8, pitches:4)
+        climb_2 = Climb.create!(name: "Guppy", lead:true, sport:true, trad:false, top_rope:false, class:5, grade:8, pitches:4)
+
+        visit '/climbs'
+
+        expect(page).to have_content(climb_1.name)
+        expect(page).to have_content(climb_2.name)
+    en2
+end
