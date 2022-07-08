e178bcfa87fd7b5f7d0721c0f7ae5abb1d1cd66b
Blake Saylor <blakesaylor@gmail.com>
2022-07-08 12:20:14 -0600

    Create test for User Story 12 (edit & update)

---
+++ spec/features/areas/edit_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe 'Edit Area' do
+    # User Story 12, Parent Update 
+    # As a visitor
+    # When I visit a parent show page
+    # Then I see a link to update the parent "Update Parent"
+    # When I click the link "Update Parent"
+    # Then I am taken to '/parents/:id/edit' where I  see a form to edit the parent's attributes:
+    # When I fill out the form with updated information
+    # And I click the button to submit the form
+    # Then a `PATCH` request is sent to '/parents/:id',
+    # the parent's info is updated,
+    # and I am redirected to the Parent's Show page where I see the parent's updated info
+    it 'I can update an area' do
+        area_1 = Area.create!(name:'Clear Creek Canyon', state:'Colorado', rock_climbing: true, elevation: 7400, latitude: 39.741, longitude: -105.41)
+
+        visit "/areas/#{area_1.id}"
+
+        click_link 'Edit Area'
+
+        expect(current_path).to eq("/areas/#{area_1.id}/edit")
+
+        fill_in 'name', with: 'Clear Creek'
+        fill_in 'state', with: 'Colorado'
+        check 'rock_climbing'
+        fill_in 'elevation', with: 7400
+        fill_in 'latitude', with: 39.741
+        fill_in 'longitude', with: -105.41
+        click_on 'Update Area'
+
+        expect(current_path).to eq("/areas/#{area_1.id}")
+        expect(page).to have_content('Clear Creek')
+    end
+end
