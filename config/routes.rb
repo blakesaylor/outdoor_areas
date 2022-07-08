8081aafec5535b8ddc1b0937c4638815a680ad11
Blake Saylor <blakesaylor@gmail.com>
2022-07-08 00:15:15 -0600

    Add routes to areas/new and creating new area

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get '/areas', to: 'areas#index'
+  get '/areas/new', to: 'areas#new'
+  post '/areas', to: 'areas#create'
   get '/areas/:id', to: 'areas#show'
   get '/climbs', to: 'climbs#index'
   get '/climbs/:id', to: 'climbs#show'
