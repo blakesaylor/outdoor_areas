36b188fd4bc934a4513204a5c941fc19eaae9967
Blake Saylor <blakesaylor@gmail.com>
2022-07-08 00:15:52 -0600

    Add methods new and create to add a new area

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -1,6 +1,6 @@
 class AreasController < ApplicationController
     def index
-        @areas = Area.all.sort_by_created_datetime
+        @areas = Area.sort_by_created_datetime
     end
 
     def show
@@ -10,5 +10,19 @@ class AreasController < ApplicationController
     def show_area_climbs
         @area = Area.find(params[:id])
         @climbs = @area.climbs
+    end
+
+    def new
+    end
+
+    def create
+        Area.create(name: params[:name],
+                    state: params[:state],
+                    rock_climbing: params[:rock_climbing],
+                    elevation: params[:elevation],
+                    latitude: params[:latitude],
+                    longitude: params[:longitude]    
+                )
+        redirect_to '/areas'
     end
 end
