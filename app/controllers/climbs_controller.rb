39fec2d64c8bd464806140aee72ed990aaa91ec9
Blake Saylor <blakesaylor@gmail.com>
2022-07-09 18:43:22 -0600

    Add methods for editing/updating a climb

--- app/controllers/climbs_controller.rb
+++ app/controllers/climbs_controller.rb
@@ -6,4 +6,25 @@ class ClimbsController < ApplicationController
     def show
         @climb = Climb.find(params[:id])
     end
+
+    def edit
+        @climb = Climb.find(params[:id])
+    end
+
+    def update
+        @climb = Climb.find(params[:id])
+        @climb.update(climb_params)
+        redirect_to "/climbs/#{@climb.id}"
+    end
+
+    private
+    def climb_params
+        params.permit(  :name, 
+                        :lead, 
+                        :sport, 
+                        :trad, 
+                        :top_rope, 
+                        :grade,
+                        :pitches)
+    end
 end
