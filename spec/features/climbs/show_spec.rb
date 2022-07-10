1c21c1cd678835f2c44c340c4e9b25db2412abcd
Blake Saylor <blakesaylor@gmail.com>
2022-07-09 18:31:31 -0600

    Add button to climbs show page to pass user story 14

--- spec/features/climbs/show_spec.rb
+++ spec/features/climbs/show_spec.rb
@@ -51,4 +51,23 @@ RSpec.describe 'climbs show by id', type: :feature do
 
         expect(page).to have_link('Areas Index', href: '/areas')
     end
+
+    # User Story 14, Child Update 
+    # As a visitor
+    # When I visit a Child Show page
+    # Then I see a link to update that Child "Update Child"
+    # When I click the link
+    # I am taken to '/child_table_name/:id/edit' where I see a form to edit the child's attributes:
+    # When I click the button to submit the form "Update Child"
+    # Then a `PATCH` request is sent to '/child_table_name/:id',
+    # the child's data is updated,
+    # and I am redirected to the Child Show page where I see the Child's updated information
+    it 'has a link to update the climb' do
+        area_1 = Area.create!(name:'Clear Creek Canyon', state:'Colorado', rock_climbing: true, elevation: 7400, latitude: 39.741, longitude: -105.41)
+        climb_1 = area_1.climbs.create!(name: "Playin' Hooky", lead:true, sport:true, trad:false, top_rope:false, grade:'5.8', pitches:4)
+
+        visit "climbs/#{climb_1.id}"
+
+        expect(page).to have_link('Update Climb', href: "/climbs/#{climb_1.id}/edit")
+    end
 end
