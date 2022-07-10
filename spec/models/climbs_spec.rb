772a1e6fb274310bb7a2ed89d0e22663de266747
Blake Saylor <blakesaylor@gmail.com>
2022-07-09 21:40:09 -0600

    Create test for show_top_ropeable model method

--- spec/models/climbs_spec.rb
+++ spec/models/climbs_spec.rb
@@ -12,4 +12,37 @@ RSpec.describe Climb, type: :model do
     describe 'relationships' do
         it {should belong_to :area }
     end
+
+    describe 'methods' do
+        describe 'show_top_ropeable' do
+            it 'returns an array of climbs in an area that are top ropeable' do
+                area_1 = Area.create!(  name:'Clear Creek Canyon', 
+                                        state:'Colorado', 
+                                        rock_climbing: true, 
+                                        elevation: 7400, 
+                                        latitude: 39.741, 
+                                        longitude: -105.41)
+
+                area_2 = Area.create!(  name:'Boulder Canyon', 
+                                        state:'Colorado', 
+                                        rock_climbing: true, 
+                                        elevation: 7126,
+                                        latitude: 40.002, 
+                                        longitude: -105.41)
+
+                climb_1 = area_1.climbs.create!(name: "Playin' Hooky",
+                                                top_rope:false, 
+                                                grade:'5.8', 
+                                                pitches:4)
+
+                climb_2 = area_1.climbs.create!(name: "Guppy", 
+                                                top_rope: true, 
+                                                grade:'5.8', 
+                                                pitches:1)
+
+                expect(area_1.climbs.show_top_ropeable.count).to eq 1
+                expect(area_1.climbs.show_top_ropeable).to eq [climb_2]
+            end
+        end
+    end
 end
