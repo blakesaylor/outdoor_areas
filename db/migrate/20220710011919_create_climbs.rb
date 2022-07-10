fdc84668a6554e9fca908278700f3300f766b444
Blake Saylor <blakesaylor@gmail.com>
2022-07-09 19:20:09 -0600

    Remove extra/unnecessary booleans from Climb

--- db/migrate/20220710011919_create_climbs.rb
+++ db/migrate/20220710011919_create_climbs.rb
@@ -2,13 +2,9 @@ class CreateClimbs < ActiveRecord::Migration[5.2]
   def change
     create_table :climbs do |t|
       t.string :name
-      t.boolean :lead
-      t.boolean :sport
-      t.boolean :trad
       t.boolean :top_rope
       t.string :grade
       t.integer :pitches
-      t.timestamps
     end
   end
 end
