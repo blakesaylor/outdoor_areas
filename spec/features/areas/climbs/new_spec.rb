b91e2d3336d31a1aaa9daed0c9b7e4b25050f8ee
Blake Saylor <blakesaylor@gmail.com>
2022-07-11 13:33:09 -0600

    Add tests for US8, 9.

--- spec/features/areas/climbs/new_spec.rb
+++ spec/features/areas/climbs/new_spec.rb
@@ -2,17 +2,65 @@ require 'rails_helper'
 
 RSpec.describe 'area climbs new' do
 
-# User Story 13, Parent Child Creation 
-# As a visitor
-# When I visit a Parent Childs Index page
-# Then I see a link to add a new adoptable child for that parent "Create Child"
-# When I click the link
-# I am taken to '/parents/:parent_id/child_table_name/new' where I see a form to add a new adoptable child
-# When I fill in the form with the child's attributes:
-# And I click the button "Create Child"
-# Then a `POST` request is sent to '/parents/:parent_id/child_table_name',
-# a new child object/row is created for that parent,
-# and I am redirected to the Parent Childs Index page where I can see the new child listed
+    # User Story 8, Child Index Link
+    # As a visitor
+    # When I visit any page on the site
+    # Then I see a link at the top of the page that takes me to the Child Index
+    it 'has a link that takes the user to the climbs index' do
+        area_1 = Area.create!(  name:'Clear Creek Canyon', 
+                                state:'Colorado', 
+                                rock_climbing: true, 
+                                elevation: 7400, 
+                                latitude: 39.741, 
+                                longitude: -105.41)
+
+        climb_1 = area_1.climbs.create!(name: "Playin' Hooky",
+                                        top_rope:false,    
+                                        grade:'5.8', 
+                                        pitches:4)
+
+        visit "/areas/#{area_1.id}/climbs/new"
+
+        expect(page).to have_link('Climbs Index')
+        click_link 'Climbs Index'
+        expect(current_path).to eq ('/climbs')
+    end
+
+    # User Story 9, Parent Index Link
+    # As a visitor
+    # When I visit any page on the site
+    # Then I see a link at the top of the page that takes me to the Parent Index
+    it 'has a link that takes the user to the areas index' do
+        area_1 = Area.create!(  name:'Clear Creek Canyon', 
+                                state:'Colorado', 
+                                rock_climbing: true, 
+                                elevation: 7400, 
+                                latitude: 39.741, 
+                                longitude: -105.41)
+
+        climb_1 = area_1.climbs.create!(name: "Playin' Hooky",
+                                        top_rope:false,    
+                                        grade:'5.8', 
+                                        pitches:4)
+
+        visit "/areas/#{area_1.id}/climbs/new"
+
+        expect(page).to have_link('Areas Index')
+        click_link 'Areas Index'
+        expect(current_path).to eq ('/areas')
+    end
+
+    # User Story 13, Parent Child Creation 
+    # As a visitor
+    # When I visit a Parent Childs Index page
+    # Then I see a link to add a new adoptable child for that parent "Create Child"
+    # When I click the link
+    # I am taken to '/parents/:parent_id/child_table_name/new' where I see a form to add a new adoptable child
+    # When I fill in the form with the child's attributes:
+    # And I click the button "Create Child"
+    # Then a `POST` request is sent to '/parents/:parent_id/child_table_name',
+    # a new child object/row is created for that parent,
+    # and I am redirected to the Parent Childs Index page where I can see the new child listed
     it 'has a form to create a new climb' do
         area_1 = Area.create!(  name:'Clear Creek Canyon', 
                                 state:'Colorado', 
