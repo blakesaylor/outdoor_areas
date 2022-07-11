5a67adf4e8afce3c6f7856eaf4ba219f6363c6fb
Blake Saylor <blakesaylor@gmail.com>
2022-07-11 13:31:13 -0600

    Fix tests based on changing to link_to

--- spec/features/areas/climbs/index_spec.rb
+++ spec/features/areas/climbs/index_spec.rb
@@ -57,7 +57,9 @@ RSpec.describe 'area climbs index', type: :feature do
 
         visit "/areas/#{area_1.id}/climbs"
 
-        expect(page).to have_link('Climbs Index', href: '/climbs')
+        expect(page).to have_link('Climbs Index')
+        click_link 'Climbs Index'
+        expect(current_path).to eq ('/climbs')
     end
 
     # User Story 9, Parent Index Link
@@ -79,7 +81,9 @@ RSpec.describe 'area climbs index', type: :feature do
 
         visit "/areas/#{area_1.id}/climbs"
 
-        expect(page).to have_link('Areas Index', href: '/areas')
+        expect(page).to have_link('Areas Index')
+        click_link 'Areas Index'
+        expect(current_path).to eq ('/areas')
     end
 
     # User Story 13, Parent Child Creation 
